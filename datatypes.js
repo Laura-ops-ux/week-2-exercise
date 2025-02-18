84337bba8bfb87f80d8f6e22d4ba84c4147d8b0f
Apiyo Laura <apiyolaura24@gmail.com>
2025-02-18 17:37:03 +0300

    javascript welcome

---
+++ datatypes.js
@@ -0,0 +1,16 @@
+//primitive - stored by their actual value
+//number - numerical character e.g 1,2,3....
+//boolean - A YES or NO e.g is logged in=True
+//undefined e.g X or Y
+//string - set of characters that form a word e.g 'egg', "egg", `egg`
+//null e.g Y=null
+//bigint eg big number=88888888888888888888
+//symbol
+
+
+
+
+//Non-primitive  database has a reference
+//array means a list
+//function
+//object, date
---
+++ index.js
@@ -0,0 +1,4 @@
+2+2
+console.log(2+2) //simple calc
+console.log("My name is Apiyo Laura"); //my name
+console.log("I am a software engineer"); //my career journey
