ea3801139e3443b56671a522daa5c8b5308edade
Apiyo Laura <apiyolaura24@gmail.com>
2025-02-24 10:11:08 +0300

    Week two progress

---
+++ week 2/conditions.js
@@ -0,0 +1,44 @@
+//if (condition){
+//This part of code runs if the condition is true
+//};
+
+let number = 6;
+if (number>0){
+    console.log(`${number} is a positive integer`)
+};
+
+
+//String
+let weather = "raining";
+if (weather ==="raining"){
+    console.log("Remember to carry an umbrella")
+};
+
+//boolean
+let isRaining = true;
+if (isRaining){
+    console.log("Rememeber to bring an umbrella")
+};
+
+
+
+
+
+let numberTwo = 6;
+if (numberTwo>0){
+    console.log(`${number} is a positive integer`)
+} else {
+    console.log(`${numberTwo} is a negative integer`)
+};
+
+let weatherNow = "sunny";
+if (weatherNow ==="rainy"){
+    console.log("You need an umbrella");
+} else if(weatherNow==="cloudy"){
+    console.log("You might need an umbrella")
+}else if(weatherNow=="sunny"){
+    console.log("wear light clothes")
+
+} else {
+    console.log("No need for an umbrella")
+}
